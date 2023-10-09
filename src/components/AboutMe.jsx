deac6abb107d9f003f1dfd8cc5de941629c614c0
ashpreetkaur25 <82947364+ashpreetkaur25@users.noreply.github.com>
2023-10-09 13:32:11 +0530

    Add files via upload

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { motion, useTransform, useScroll } from "framer-motion";
+
+import Wrapper from "./Wrapper";
+// import flag from "../assets/indian-flag.png";
+import pe1 from "../assets/sec-2-p-e-1.png";
+import pe2 from "../assets/sec-2-p-e-2.png";
+import Div from "./Div";
+
+const AboutMe = () => {
+    const { scrollY } = useScroll();
+    const y1 = useTransform(scrollY, [0, 500], [100, 0], { clamp: false });
+    const y2 = useTransform(scrollY, [0, 500], [0, -200], { clamp: false });
+    return (
+        <div
+            id="about"
+            className="w-full py-[50px] md:py-[100px] bg-white text-black relative overflow-hidden"
+        >
+            {/* BACKGROUND ELEMENTS START */}
+            <span className="sec-2-bg-gradient" />
+            <motion.img className="sec-2-p-e-1" style={{ y: y1 }} src={pe1} />
+            <motion.img className="sec-2-p-e-2" style={{ y: y2 }} src={pe2} />
+            {/* BACKGROUND ELEMENTS END */}
+
+            <Wrapper>
+                {/* HEADING START */}
+                <Div className="text-[40px] md:text-[90px] 2xl:text-[120px] leading-[52px] md:leading-[95px] 2xl:leading-[123px] font-oswald uppercase mb-[25px] md:mb-[40px] 2xl:mb-[60px] flex flex-col relative">
+                    <span>A Computer Science </span>
+                    <span className="flex items-center gap-2">
+                        <span>Graduate</span>
+                        {/* <img
+                            src={flag}
+                            alt=""
+                            className="w-[70px] md:w-[150px] 2xl:w-[190px] block mt-1 md:mt-2"
+                        />
+                        <span>In India</span> */}
+                    </span>
+                </Div>
+                {/* HEADING END */}
+
+                {/* PARAGRAPH START */}
+                <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                Welcome to my portfolio! I'm a Computer Science graduate with a fervent passion for technology. My primary interests include Machine Learning and Artificial Intelligence, where I've applied these concepts to practical problem-solving. My proficiency extends to web development, where I've created responsive and visually engaging websites. Additionally, I'm enthusiastic about research work, continuously pushing the boundaries of technology. This portfolio showcases a glimpse of my work, highlighting my dedication to these domains. If you share my enthusiasm for technology and innovation, let's connect and collaborate on exciting projects together!
+                </Div>
+                {/* PARAGRAPH END */}
+
+                
+                {/* <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] mb-[20px] md:mb-[30px] 2xl:mb-[50px] relative">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it
+                </Div> */}
+                {/* PARAGRAPH END */}
+
+                {/* PARAGRAPH START */}
+                {/* <Div className="max-w-[759px] 2xl:max-w-[959px] text-[16px] 2xl:text-[20px] leading-[24px] 2xl:leading-[32px] relative">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled itLorem Ipsum is
+                    simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever
+                    since the 1500s, when an unknown printer took a galley of
+                    type and scrambled it
+                </Div> */}
+                {/* PARAGRAPH END */}
+            </Wrapper>
+        </div>
+    );
+};
+
+export default AboutMe;
---
+++ src/data.js
@@ -0,0 +1,82 @@
+import p1 from "./assets/project-1.png";
+import p2 from "./assets/project-2.png";
+import p3 from "./assets/project-3.png";
+import p4 from "./assets/project-4.png";
+import p5 from "./assets/project-5.png";
+import p6 from "./assets/project-6.png";
+
+export const filters = [
+    {
+        name: "All",
+        id: "all",
+    },
+    {
+        name: "Research",
+        id: "Research",
+    },
+    {
+        name: "Website",
+        id: "Website",
+    },
+    {
+        name: "Machine Learning",
+        id: "Machine Learning",
+    },
+    
+];
+
+export const projects = [
+    {
+        name: "Visual Dreams: AI-Powered Image Synthesis",
+        image: p1,
+        tags: ["website"],
+    },
+    {
+        name: "Enhancing Topic Prediction using Machine Learning Techniques and Concept net based Cosine Similarity",
+        image: p2,
+        tags: ["Research", "Machine Learning"],
+    },
+    {
+        name: "An Optimized Textual Document Categorization Based on FFT",
+        image: p3,
+        tags: ["Research", "Machine Learning"],
+    },
+    {
+        name: "Artistic Alchemy",
+        image: p4,
+        tags: ["Machine Learning"],
+    },
+    {
+        name: "Budget Tracker",
+        image: p5,
+        tags: ["Website"],
+    },
+    {
+        name: "News App",
+        image: p6,
+        tags: ["Website"],
+    },
+];
+
+export const achievements = [
+    {
+        id: 1,
+        year: 10,
+        description: "SUCCESSFULLY<br />COMPLETED<br />PROJECTS",
+    },
+    {
+        id: 2,
+        year: 2,
+        description: "RESEARCH<br />PAPERS",
+    },
+    {
+        id: 3,
+        year: 25,
+        description: "TECHNOLOGIES",
+    },
+    {
+        id: 4,
+        year: 100,
+        description: "GITHUB<br />CONTRIBUTIONS",
+    },
+];
